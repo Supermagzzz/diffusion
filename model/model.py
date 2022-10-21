1d44ed58440f85f3027d4705cd209fde8190678d
Maxim Kuzin <12345min@mail.ru>
2022-10-22 00:42:07 +0300

    fix

--- model/model.py
+++ model/model.py
@@ -143,8 +143,8 @@ class SimpleDenoiser(nn.Module):
         svg = svg.reshape(batch_size, N * M // 6, 6)
         svg = torch.clamp((svg + 1) / 2 * BLOCKS, 0, BLOCKS - 1).long()
 
-        coords = F.one_hot(svg, BLOCKS)
-        coords = torch.matmul(coords.to('cpu'), self.w_x).to('device')
+        coords = F.one_hot(svg, BLOCKS).float()
+        coords = torch.matmul(coords.to('cpu'), self.w_x).to(self.device)
         coords = coords.reshape(batch_size, N * M // 6, HIDDEN * 6)
         embeds = torch.matmul(coords, self.w_coords)
         noise_embeds = self.transformer(embeds, embeds)
