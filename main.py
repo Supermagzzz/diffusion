6055dc81e099770c2925baf0c4cefed9c2e675a9
Maxim Kuzin <12345min@mail.ru>
2022-10-24 23:08:06 +0300

    fix

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import math
-import sys
-
-from deepsvg.svglib.svg import SVG
-
-import pydiffvg
 import torch
-import numpy as np
-from torch.optim import Adam
-import torch.nn.functional as F
-from torch.nn.functional import l1_loss
 
 from dataset.dataloader import CustomImageDataset
 from torch.utils.data import DataLoader
@@ -16,56 +6,20 @@ from model.model import SimpleDenoiser
 
 torch.set_default_dtype(torch.float32)
 noise_level = 0.03
+know_level = 0.01
+batch_sz = 100
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 dataset = CustomImageDataset('data/tensors')
-dataloader = DataLoader(dataset, batch_size=100 if device == "cpu" else 100, shuffle=False, drop_last=True)
+dataloader = DataLoader(dataset, batch_size=batch_sz if device == "cpu" else batch_sz, shuffle=False, drop_last=True)
 
 
 def add_noise(tensor, mult):
-    return torch.normal(0, mult, size=tensor.shape).to(device) - tensor * 0.01
+    return torch.normal(0, mult, size=tensor.shape).to(device) - tensor * know_level
+
 
 N = 5
-M = 20
-
-def make_image(inp):
-    # return el
-    canvas_width, canvas_height = 64, 64
-    el = (inp + 0.5)
-    el *= int(0.8 * canvas_width)
-    el += int(0.1 * canvas_width)
-
-    all_ims = []
-    pathes = []
-    groups = []
-    for j, row in enumerate(el):
-        for i in range(6, row.shape[0], 6):
-            num_control_points = []
-            points = []
-            num_control_points.append(2)
-            points.append(i - 2)
-            points.append(i - 1)
-            points.append(i + 2)
-            points.append(i + 3)
-            points.append(i)
-            points.append(i + 1)
-            points.append(i + 4)
-            points.append(i + 5)
-            points = row[points].reshape(-1, 2)
-        pathes.append(pydiffvg.Path(torch.Tensor(num_control_points).to(device), points, False))
-        groups.append(pydiffvg.ShapeGroup(shape_ids=torch.tensor([len(groups)]).to(device), fill_color=None,
-                                          stroke_color=torch.Tensor([0, 0, 0, 1]).to(device)))
-    scene_args = pydiffvg.RenderFunction.serialize_scene(canvas_width, canvas_height, pathes, groups)
-    render = pydiffvg.RenderFunction.apply
-    img = render(canvas_width,  # width
-                 canvas_height,  # height
-                 2,  # num_samples_x
-                 2,  # num_samples_y
-                 1,  # seed
-                 None,
-                 *scene_args)
-    return img
-
+M = 8
 
 model = SimpleDenoiser(noise_level, device)
 model.to(device)
@@ -74,8 +28,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 new_img, noise = None, None
 for epoch in range(100000):
     for step, batch in enumerate(dataloader):
-        batch = torch.cat([batch, batch[:, :, -2:], batch[:, :, -2:]], dim=-1)
-        batch = batch[:, 0:1, :]
+        batch = torch.cat([batch[:, :, :2], batch[:, :, :2], batch], dim=-1)
         optimizer.zero_grad()
         batch = batch.to(device)
         noise = add_noise(batch, noise_level).to(device)
@@ -86,7 +39,7 @@ for epoch in range(100000):
             return (a - b).pow(2).sum()
 
         loss = gloss(noise, pred_noise)
-        baseline = gloss(noise, noise * 0)
+        baseline = gloss(noise, -batch * know_level)
         loss.backward()
         optimizer.step()
         print(epoch, loss.item(), baseline.item(), (loss / baseline).item())
--- model/model.py
+++ model/model.py
@@ -3,37 +3,13 @@ from torch import nn
 import torch.nn.functional as F
 import math
 
-M_REAL = 20
+M_REAL = 8
 M = 6 + 6 * M_REAL
-N = 1
+N = 5
 IMG_N = 50
 HIDDEN = 128
 M_DIV = 1
-BLOCKS = 100000
-# [-0.5, 0.5] -> int
-
-class Block(nn.Module):
-    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
-        super().__init__()
-        self.time_mlp = nn.Linear(time_emb_dim, out_ch)
-        if up:
-            self.conv1 = nn.Conv2d(2 * in_ch, out_ch, 3, padding=1)
-            self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)
-        else:
-            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
-        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bnorm1 = nn.BatchNorm2d(out_ch)
-        self.bnorm2 = nn.BatchNorm2d(out_ch)
-        self.relu = nn.ReLU()
-
-    def forward(self, x, t):
-        h = self.bnorm1(self.relu(self.conv1(x)))
-        time_emb = self.relu(self.time_mlp(t))
-        time_emb = time_emb[(...,) + (None,) * 2]
-        h = h + time_emb
-        h = self.bnorm2(self.relu(self.conv2(h)))
-        return self.transform(h)
+BLOCKS = 1000000
 
 
 class SinusoidalPositionEmbeddings(nn.Module):
@@ -55,8 +31,8 @@ class SimpleDenoiser(nn.Module):
     def __init__(self, noise_level, device):
         super().__init__()
         self.device = device
-        self.w_x = nn.Parameter(torch.empty(BLOCKS, HIDDEN), requires_grad=True).to('cpu')
-        self.w_coords = nn.Parameter(torch.empty(HIDDEN * 6, HIDDEN), requires_grad=True)
+        self.w_x = nn.Parameter(torch.normal(0, 1, (BLOCKS * 6, HIDDEN)), requires_grad=True).to('cpu')
+        self.w_coords = nn.Parameter(torch.normal(0, 1, (HIDDEN * 6, HIDDEN)), requires_grad=True)
         self.transformer = nn.Transformer(d_model=HIDDEN, dtype=torch.float)
         self.make_coord_embed = nn.Sequential(
             nn.Linear(HIDDEN, HIDDEN * 6).to(device),
@@ -73,113 +49,16 @@ class SimpleDenoiser(nn.Module):
             nn.Linear(HIDDEN, 1).to(device)
         )
 
-        # self.make_coords = nn.Sequential(
-        #     nn.Linear(HIDDEN, 2 * BLOCKS),
-        #     nn.Softmax(dim=1),
-        #     nn.Linear(2 * BLOCKS, 1),
-        # )
-
-        # self.image_channels = 4 * N * ((M - 2) // 6) // M_DIV
-        # self.input_sz = 64
-        # self.hidden = 32
-        #
-        # image_channels = self.image_channels
-        # input_sz = self.input_sz
-        # down_channels = (64, 128, 256, 512, 1024)
-        # up_channels = (1024, 512, 256, 128, 64)
-        # out_dim = 1
-        # time_emb_dim = 32
-        #
-        # self.time_mlp = nn.Sequential(
-        #     SinusoidalPositionEmbeddings(time_emb_dim),
-        #     nn.Linear(time_emb_dim, time_emb_dim),
-        #     nn.ReLU()
-        # )
-        # self.prepareSvg = nn.Sequential(
-        #     nn.Linear(N * M, input_sz * input_sz),
-        #     nn.ReLU()
-        # )
-        #
-        # self.conv0 = nn.Conv2d(image_channels + 1, down_channels[0], 3, padding=1)
-        # self.downs = nn.ModuleList(
-        #     [Block(down_channels[i], down_channels[i + 1], time_emb_dim) for i in range(len(down_channels) - 1)]
-        # )
-        # self.ups = nn.ModuleList(
-        #     [Block(up_channels[i], up_channels[i + 1], time_emb_dim, True) for i in range(len(up_channels) - 1)]
-        # )
-        # self.output = nn.Sequential(
-        #     nn.Conv2d(up_channels[-1], image_channels, out_dim),
-        #     nn.ReLU()
-        # )
-        #
-        # self.svgLinear = nn.Sequential(
-        #     nn.Linear(N * M, 4 * N * M),
-        #     nn.ReLU()
-        # )
-        #
-        # self.pngBigger = nn.Sequential(
-        #     nn.Linear(M_REAL // M_DIV, M)
-        # )
-        # self.pngLinear = nn.Sequential(
-        #     nn.Linear(input_sz * input_sz, self.hidden),
-        #     nn.ReLU()
-        # )
-        #
-        # self.linearWithSvg = nn.Sequential(
-        #     nn.Linear(4 * N * M * (self.hidden + 1), 4 * N * M),
-        #     nn.ReLU(),
-        #     nn.Linear(4 * N * M, N * M),
-        #     nn.ReLU(),
-        #     nn.Linear(N * M, N * M)
-        # )
-
-        self.t = nn.Sequential(
-            nn.Linear(HIDDEN, HIDDEN).to(device),
-            nn.ReLU().to(device),
-            nn.Linear(HIDDEN, 6).to(device)
-        )
-
-
     def forward(self, svg, timestep):
         batch_size = svg.shape[0]
         svg = svg.reshape(batch_size, N * M // 6, 6)
         svg = torch.clamp((svg + 1) / 2 * BLOCKS, 0, BLOCKS - 1).long()
-        coords = F.embedding(svg.to('cpu'), self.w_x).to(self.device)
+        coords = F.embedding(svg, self.w_x).to(self.device)
         coords = coords.reshape(batch_size, N * M // 6, HIDDEN * 6)
         embeds = torch.matmul(coords, self.w_coords)
-        return self.t(embeds).reshape(-1, N, M)
         noise_embeds = self.transformer(embeds, embeds)
         coord_embed = self.make_coord_embed(noise_embeds)
         coord_embed = coord_embed.reshape(batch_size, N * M, HIDDEN)
         # bin_probs = torch.softmax(torch.matmul(coord_embed.to('cpu'), self.w_x.permute(1, 0)), dim=-1).to(self.device)
         noise_result = self.make_noise_result(coord_embed)
         return noise_result.reshape(batch_size, N, M)
-
-        # t = self.time_mlp(timestep)
-        # prep_svg = self.prepareSvg(svg.reshape(-1, N * M))
-        # prep_svg = prep_svg.reshape(-1, self.input_sz, self.input_sz, 1)
-        # x = torch.cat((x, prep_svg), dim=-1)
-        # x = x.permute(0, 3, 1, 2)
-        # x = self.conv0(x)
-        # residual_inputs = []
-        # for down in self.downs:
-        #     x = down(x, t)
-        #     residual_inputs.append(x)
-        # for up in self.ups:
-        #     residual_x = residual_inputs.pop()
-        #     x = torch.cat((x, residual_x), dim=1)
-        #     x = up(x, t)
-        # x = self.output(x)
-        # x = x.reshape(-1, x.shape[1], x.shape[2] * x.shape[3])
-        #
-        # svg = self.svgLinear(svg.reshape(-1, N * M)).reshape(-1, 4 * N * M, 1)
-        #
-        # png = x.reshape(-1, 4 * N, M_REAL // M_DIV, self.input_sz * self.input_sz)
-        # png = png.permute(0, 1, 3, 2)
-        # png = self.pngBigger(png)
-        # png = png.permute(0, 1, 3, 2)
-        # png = self.pngLinear(png)
-        # png = png.reshape(-1, 4 * N * M, self.hidden)
-        # all = torch.cat([svg, png], dim=-1).reshape(-1, 4 * N * M * (self.hidden + 1))
-        #
-        # return self.linearWithSvg(all).reshape(-1, N, M)
