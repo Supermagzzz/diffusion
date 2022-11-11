8e75668df83c64bcbca78e30d2aa7fb306e18a33
Maxim Kuzin <12345min@mail.ru>
2022-11-11 17:22:24 +0300

    fix

--- model/model.py
+++ model/model.py
@@ -25,17 +25,15 @@ class SimpleDenoiser(nn.Module):
         self.common = common
         self.device = common.device
         self.add_time_embed_table = nn.Embedding(common.T, common.HIDDEN)
-
         self.get_time_embed_table_normal = nn.Embedding(common.T, common.HIDDEN)
         self.get_time_embed_table_sinus = SinusoidalPositionEmbeddings(common.HIDDEN)
         self.get_time_embed_table = nn.Sequential(
-            nn.Linear(common.HIDDEN * 4, common.HIDDEN),
+            nn.Linear(common.HIDDEN * 3, common.HIDDEN),
             nn.ReLU(),
             nn.Linear(common.HIDDEN, common.HIDDEN),
             nn.ReLU(),
             nn.Linear(common.HIDDEN, common.HIDDEN)
         )
-
         self.pos_embed_table = nn.Sequential(
             SinusoidalPositionEmbeddings(common.HIDDEN),
             nn.Linear(common.HIDDEN, common.HIDDEN),
@@ -49,29 +47,32 @@ class SimpleDenoiser(nn.Module):
             nn.ReLU(),
             nn.Linear(common.HIDDEN, common.HIDDEN)
         )
-        self.w_coords = nn.Linear(common.HIDDEN * 6, common.HIDDEN)
+        self.w_coords = nn.Sequential(
+            nn.Linear(common.HIDDEN * 6, common.HIDDEN),
+            nn.ReLU()
+        )
         self.unite_with_embeds = nn.Sequential(
-            nn.Linear(common.HIDDEN * 3, common.HIDDEN),
+            nn.Linear(common.HIDDEN * 4, common.HIDDEN),
             nn.ReLU(),
             nn.Linear(common.HIDDEN, common.HIDDEN),
             nn.ReLU(),
             nn.Linear(common.HIDDEN, common.HIDDEN),
         )
-        # self.transformer = nn.Transformer(d_model=common.HIDDEN, dtype=torch.float, batch_first=True)
-        self.transformer = T5ForConditionalGeneration(T5Config(common.BLOCKS, common.HIDDEN))
+        self.transformer = nn.Transformer(d_model=common.HIDDEN, dtype=torch.float, batch_first=True)
+        # self.transformer = T5ForConditionalGeneration(T5Config(common.BLOCKS))
         # self.make_probs = nn.Sequential(
         #     nn.Softmax(),
         #     nn.Linear(common.BLOCKS, common.HIDDEN),
         #     nn.ReLU()
         # )
-        self.make_coord_embed = nn.ModuleList([nn.Linear(common.BLOCKS, common.HIDDEN * 6)] + sum([[
+        self.make_coord_embed = nn.ModuleList([nn.Linear(common.HIDDEN * 2, common.HIDDEN * 6), nn.Tanh()] + sum([[
             nn.Linear(common.HIDDEN * 6, common.HIDDEN * 6),
             nn.ReLU()
-        ] for i in range(3)], []) + [nn.Linear(common.HIDDEN * 6, common.HIDDEN * 6)])
-        self.make_noise_result = nn.ModuleList([nn.Linear(common.HIDDEN, common.HIDDEN)] + sum([[
+        ] for i in range(2)], []) + [nn.Linear(common.HIDDEN * 6, common.HIDDEN * 6), nn.ReLU()])
+        self.make_noise_result = nn.ModuleList([nn.Linear(common.HIDDEN * 3, common.HIDDEN), nn.Tanh()] + sum([[
             nn.Linear(common.HIDDEN, common.HIDDEN),
             nn.ReLU()
-        ] for i in range(3)], []) + [nn.Linear(common.HIDDEN, 1)])
+        ] for i in range(2)], []) + [nn.Linear(common.HIDDEN, 1)])
 
     def make_seq(self, data, embeds):
         data = data.reshape(embeds.shape[0], 1, self.common.HIDDEN)
@@ -79,9 +80,6 @@ class SimpleDenoiser(nn.Module):
 
     def forward(self, svg, timestamp):
         batch_size = svg.shape[0]
-
-        svg_ids = torch.clamp((svg.reshape(batch_size, -1) + self.range) / (2 * self.range) * self.common.BLOCKS, 0, self.common.BLOCKS - 1).long()
-
         svg = svg.reshape(batch_size, self.common.N * self.common.M_REAL // 6, 6)
         svg_long = torch.clamp((svg + self.range) / (2 * self.range) * self.common.BLOCKS, 0, self.common.BLOCKS - 1).long()
         svg_rem = torch.fmod((svg + self.range) / (2 * self.range) * self.common.BLOCKS, 1)
@@ -107,8 +105,10 @@ class SimpleDenoiser(nn.Module):
             pos_embed
         ], dim=-1))
 
-        noise_embeds = self.transformer(input_ids=svg_ids, decoder_inputs_embeds=out_embeds).logits
-        coord_embed = noise_embeds
+        svg = svg.reshape(batch_size, -1)
+        svg_long = torch.clamp((svg + self.range) / (2 * self.range) * self.common.BLOCKS, 0, self.common.BLOCKS - 1).long()
+        noise_embeds = self.transformer(embeds, out_embeds)
+        coord_embed = torch.cat([noise_embeds, noise_embeds], dim=-1)
         for layer in self.make_coord_embed:
             coord_embed = layer(coord_embed)
         coord_embed = coord_embed.reshape(batch_size, self.common.N * self.common.M_REAL, self.common.HIDDEN)
@@ -116,7 +116,7 @@ class SimpleDenoiser(nn.Module):
         # prob_embeds = self.make_probs(torch.matmul(self.w_x, coord_embed.permute(0, 2, 1)).permute(0, 2, 1))
         # coord_prob_embeds = coord_embed * prob_embeds
         # noise_result = torch.cat([coord_embed, prob_embeds, coord_prob_embeds], dim=-1)
-        noise_result = coord_embed
+        noise_result = torch.cat([coord_embed, coord_embed, coord_embed], dim=-1)
         for layer in self.make_noise_result:
             noise_result = layer(noise_result)
         return noise_result.reshape(batch_size, self.common.N, self.common.M_REAL)
